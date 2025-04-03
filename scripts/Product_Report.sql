c1acc633df394f35e7662c97dcc4db635df67a95
Reet <reetphy@gmail.com>
2025-04-03 19:13:37 +0530

    Update Product_Report.sql

--- scripts/Product_Report.sql
+++ scripts/Product_Report.sql
@@ -32,9 +32,9 @@ WITH base_query AS (
 1) Base Query: Retrieves core columns from fact_sales and dim_products
 ---------------------------------------------------------------------------*/
     SELECT
-	    f.order_number,
+	f.order_number,
         f.order_date,
-		f.customer_key,
+	f.customer_key,
         f.sales_amount,
         f.quantity,
         p.product_key,
@@ -61,10 +61,10 @@ SELECT
     TIMESTAMPDIFF(MONTH, MIN(order_date), MAX(order_date)) AS lifespan,
     MAX(order_date) AS last_sale_date,
     COUNT(DISTINCT order_number) AS total_orders,
-	COUNT(DISTINCT customer_key) AS total_customers,
+    COUNT(DISTINCT customer_key) AS total_customers,
     SUM(sales_amount) AS total_sales,
     SUM(quantity) AS total_quantity,
-	ROUND(AVG(CAST(sales_amount AS FLOAT) / NULLIF(quantity, 0)),1) AS avg_selling_price
+    ROUND(AVG(CAST(sales_amount AS FLOAT) / NULLIF(quantity, 0)),1) AS avg_selling_price
 FROM base_query
 GROUP BY
     product_key,
@@ -84,7 +84,7 @@ SELECT
 	subcategory,
 	cost,
 	last_sale_date,
-    TIMESTAMPDIFF(MONTH, last_sale_date, CURDATE()) AS recency,
+        TIMESTAMPDIFF(MONTH, last_sale_date, CURDATE()) AS recency,
 	CASE
 		WHEN total_sales > 50000 THEN 'High-Performer'
 		WHEN total_sales >= 10000 THEN 'Mid-Range'
